04549264ad61cf9323238c954512dcb0a6fbe97d
minseok0507 <jms892812@gmail.com>
2024-03-15 10:51:27 +0900

    minseok

---
+++ workAll/JAVAwork/day0315/src/day0315/Ex1_DateType.java
@@ -0,0 +1,19 @@
+package day0315;
+
+public class Ex1_DateType {
+    public static void main(String[] args){
+    //주석
+        //기본 자료형 공부
+        //구역 ({}) 안에서 선언하는 변수들은 자동초기화가 되지않은므로
+        //초기값을 지정하거나 나중에 변수에 값을 전달해야 출력이 가능하다.
+
+        boolean flag = true;
+        boolean flag2;
+        flag2 = false;
+
+        System.out.println(flag);
+        System.out.println(flag2);
+        System.out.println(!flag);
+        System.out.println(!flag2);
+    }
+}
