93cdc8442e3e93142a84691e25fa6526699cd8a4
minseok0507 <jms892812@gmail.com>
2024-03-19 17:27:17 +0900

    3월 19일 수업

---
+++ workAll/JAVAwork/dayClass/src/day0319/Ex11_PracticeString.java
@@ -0,0 +1,35 @@
+package day0319;
+
+import java.util.Scanner;
+
+public class Ex11_PracticeString {
+    public static void main(String[] args) {
+        String msg;
+        int big = 0, small = 0, num = 0, other = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("문자 입력");
+        msg = sc.nextLine();
+
+        for (int i = 0; i < msg.length(); i++) {
+            if (msg.charAt(i) >= 'A' && msg.charAt(i) <= 'Z'){
+                big++;
+            } else if (msg.charAt(i) >= 'a' && msg.charAt(i) <= 'z'){
+                small++;
+            } else if (msg.charAt(i) >= '0' && msg.charAt(i) <= '9'){
+                num++;
+            } else {
+                other++;
+            }
+        }
+
+        System.out.printf("""
+                %s
+                대문자 : %d
+                소문자 : %d
+                숫 자 : %d
+                기 타 : %d
+                """,msg,big,small,num,other);
+
+    }
+}
